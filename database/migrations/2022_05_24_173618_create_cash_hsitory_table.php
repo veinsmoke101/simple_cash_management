62ddb2e7c03a517041e47167474c81e23ca9ff08
TahaLechgar <tahamr08@gmail.com>
2022-05-24 18:49:05 +0100

    create cash_history table migration

---
+++ database/migrations/2022_05_24_173618_create_cash_hsitory_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cash_history', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->enum('type', ['recette', 'depense']);
+            $table->decimal('transaction_amount', 8, 2);
+            $table->decimal('balance', 8, 2);
+            $table->date('created_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cash_history');
+    }
+};
